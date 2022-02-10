89c943befffd9efb8f8faebe3eff55aeec1fe625
C Plus Plus Software Engineer <cppswengr@eVantageGroup.com>
2022-02-10 16:13:18 -0600

    Initial commit

---
+++ scraper.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'watir'
+require 'webdrivers'
+require 'nokogiri'
+
+browser = Watir::Browser.new
+browser.goto 'https://blog.eatthismuch.com/latest-articles'
+parsed_page = Nokogiri::HTML(browser.html)
+
+File.open('parsed.txt', 'w') { |f| f.write parsed_page.to_s }
+
+browser.close
+
+puts parsed_page.title
+
+links = parsed_page.css('a')
+links.map { |element| element['href'] }
+
+puts links
